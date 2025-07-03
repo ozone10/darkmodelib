f4d8b9b7f5154679c15494f87772b3cec7781b16
ozone10 <positronix10@gmail.com>
2025-07-03 15:59:18 +0200

    Add demo project

---
+++ Win32DarkmodelibGUIDemo/darkmodelibdemo.h
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MPL-2.0
+
+/*
+ * Copyright (c) 2025 oZone10
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include "resource.h"
+
+#include <windows.h>
+
+#include <commctrl.h>
+#include <uxtheme.h>
+#include <vssym32.h>
+
+#include <array>
+#include <string>
+
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE hInstance, int nCmdShow, HWND& hMain);
+LRESULT CALLBACK    WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+INT_PTR CALLBACK    About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
---
+++ Win32DarkmodelibGUIDemo/resource.h
@@ -0,0 +1,33 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by darkmodelibdemo.rc
+//
+#define IDC_MYICON                      2
+#define IDD_DEMO_DIALOG                 102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_DEMO                        107
+#define IDI_SMALL                       108
+#define IDC_DEMO                        109
+#define IDR_MAINFRAME                   128
+#define IDD_DARK_FONT_DIALOG            1000
+#define IDM_CHOOSECOLOR                 32771
+#define IDM_CHOOSEFONT                  32772
+#define IDM_DARK                        32773
+#define IDM_LIGHT                       32774
+#define IDM_CLASSIC                     32775
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32779
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
